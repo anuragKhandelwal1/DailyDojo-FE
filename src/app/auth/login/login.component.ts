c035570dae1a3103cc2bfbbfc4f818597a0112e1
Anurag Khandelwal <akhandelwal@unifocus.com>
2025-06-02 00:57:06 +0530

    Create auth - login and register

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,18 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, MatSnackBarModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
@@ -24,8 +30,25 @@ export class LoginComponent {
 
   onSubmit() {
     if (this.loginForm.valid) {
-      console.log('Form Submitted:', this.loginForm.value);
-      // TODO: Call auth API here
+      const { email, password } = this.loginForm.value;
+      this.authService.login({ email, password }).subscribe({
+        next: (res) => {
+          this.showMessage('Logged In!')
+          localStorage.setItem('token', res.access_token);
+          this.router.navigate(['/dashboard']); // or your desired route
+        },
+        error: () => {
+          this.showMessage('Something went wrong. Please try again.');
+        }
+      });
     }
+  }
+
+  showMessage(message: string) {
+    this.snackBar.open(message, 'Close', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
   }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,26 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-register',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, MatSnackBarModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
   registerForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {
     this.registerForm = this.fb.group({
-      username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', Validators.required],
     }, { validators: this.passwordMatchValidator });
   }
 
-  get username() { return this.registerForm.get('username')!; }
   get email() { return this.registerForm.get('email')!; }
   get password() { return this.registerForm.get('password')!; }
   get confirmPassword() { return this.registerForm.get('confirmPassword')!; }
@@ -34,7 +39,28 @@ export class RegisterComponent {
   onSubmit() {
     if (this.registerForm.valid) {
       console.log('Registration Form:', this.registerForm.value);
-      // TODO: Send data to backend (NestJS API)
+      const { email, password } = this.registerForm.value;
+      this.authService.register({ email, password }).subscribe({
+        next: () => {
+           this.showMessage('User Registered Succesfully');
+          this.router.navigate(['/auth/login']);
+        },
+        error: (err) => {
+          if (err.status === 409) {
+            this.showMessage(err.error.message || 'Email is already registered.');
+          } else {
+            this.showMessage('Something went wrong. Please try again.');
+          }
+        }
+      });
     }
+  }
+
+  showMessage(message: string) {
+    this.snackBar.open(message, 'Close', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
   }
 }
